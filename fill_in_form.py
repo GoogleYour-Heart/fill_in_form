be9a686b418182f262ba9e5d0750d1f160c6fbfc
Deric <54764601+DericZhao@users.noreply.github.com>
2022-06-15 23:44:56 +0800

    Update fill_in_form.py

--- fill_in_form.py
+++ fill_in_form.py
@@ -8,3 +8,15 @@ resp=urllib.request.urlopen(req)
 data=resp.read().decode('utf-8')
 
 print(data)
+
+
+# latitude = '35.50154'
+# longitude = '112.85352'
+# 
+# url = 'https://www.informationofdum.com/DMU_WEB/student_5/info/?jsonnumber=%201120200289&jsonname=%E5%88%98%E6%AC%A3%E9%80%B8&jsonclass=2020%E7%BA%A7%E7%A1%95%E5%A3%AB%E7%A0%94%E7%A9%B6%E7%94%9F%E4%B8%AD%E9%98%9F&morning=36.0%E2%84%83&afternoon=36.0%E2%84%83&night=36.0%E2%84%83&jsonbody=1&jsonbodychangeinfo=&textarea=%E5%AE%B6&textprople=%E7%88%B6%E6%AF%8D&jsontouch=1&jsontouchchangeinfo=0&jsonisolate=1&jsonisolatechangeinfo=0&latitude='+ latitude +'&longitude=' + longitude+'%20HTTP/1.1'
+# 
+# req=urllib.request.Request(url)
+# resp=urllib.request.urlopen(req)
+# data=resp.read().decode('utf-8')
+# 
+# print(data)
